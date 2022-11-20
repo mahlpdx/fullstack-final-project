69bd728d03c2dacecfa867b2bde15a382c819f9e
Drew Mahler <drewmahler@Drews-MBP-2.attlocal.net>
2022-11-19 21:21:20 -0800

    Connected to React app to node backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import logo from "./logo.svg";
+import './index.css'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" 
+             alt="logo" />
+          
+        <form action="../../post" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+        </form>
+      </header>
+    </div>
+
   );
 }
 
