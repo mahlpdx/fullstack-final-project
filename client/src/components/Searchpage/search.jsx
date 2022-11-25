103abba0d46c56aaa8ae65b4c1e7bbe764fe907b
Al1999 <aya9@pdx.edu>
2022-11-25 11:18:34 -0800

    new changes in front-end

--- client/src/components/Searchpage/search.jsx
+++ client/src/components/Searchpage/search.jsx
@@ -9,11 +9,11 @@ const Search = () => {
 
     const [genre, setGenre] = useState(false);
     const handleClick2 = () => setGenre(!genre);
-
+   
     const [hot, setHot] = useState(false);
     const handleClick3 = () => setHot(!hot);
     const [namea, setNamea] = useState('');
-
+    const [view, setView] = useState(false);
     const [pic, setPic] = useState('');
     const [artist, setArtist] = useState('');
     const [followers, setFollowers] = useState('');
@@ -61,6 +61,7 @@ const Search = () => {
         setFollowers('');
         setTracks([]);
         setGenres([]);
+        setView(false);
     }
     const getArtist = async (name) => {
         try {
@@ -75,7 +76,8 @@ const Search = () => {
           setFollowers(jsonData.followers.total);
           setTracks(jsonData.tracks);
           setGenres(jsonData.genres);
-          setPop(jsonData.popularity)
+          setPop(jsonData.popularity);
+          setView(true);
           console.log(pic);
         } catch (error) {
           console.error(error.message);
@@ -127,13 +129,13 @@ const Search = () => {
                             <div className='font-geomatik text-[#df2027] '> Search by Artist Name</div>
                             <div className='flex flex-row '>
                                 <input required type='text' id="name" name="name" placeholder='artist`s name'
-                                className='p-3  font-geomatik bg-white text-zinc-900' onChange={(ev) => setNamea(ev.target.value)} />
+                                className='p-3  font-geomatik bg-white text-zinc-900' value={namea} onChange={(ev) => setNamea(ev.target.value)} />
                                 <button onClick={() => getArtist(namea)} className='p-3  font-geomatik bg-black
                                  text-white px-7 hover:bg-blue-800'>search</button>
                                 <button onClick={() => reset()} className='p-3  font-geomatik bg-[#df2027] text-white
                                  px-7 hover:bg-red-800'>Reset search</button>
                             </div>
-                            <div className='bg-cover bg-brightness-40 w-full h-full rounded-3xl mt-3 'style={{backgroundImage: `url(${pic})`}} >
+                            <div className={!view ? 'hidden':'bg-cover bg-brightness-40 w-full h-full rounded-3xl mt-3 '}style={{backgroundImage: `url(${pic})`}} >
                                 <div className='backdrop-blur-sm flex flex-col items-start mb-9 rounded-3xl bg-zinc-900/50 w-full h-full p-9'>
                                     <div className='flex flex-row items-center mb-9 rounded-3xl bg-zinc-900/80 w-full h-full p-9'>
                                         <img src={pic} className=' w-[15rem] h-[15rem] rounded-xl mr-12'/>
