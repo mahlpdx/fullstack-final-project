6123cf07fd7c74f2caa8850a39eecd12f39a27e7
Al1999 <aya9@pdx.edu>
2022-11-26 13:49:05 -0800

    added new changes to front-end

--- client/src/components/Searchpage/searchpanel.jsx
+++ client/src/components/Searchpage/searchpanel.jsx
@@ -112,17 +112,18 @@ const Artistname = () => {
           console.error(error.message);
         }
       };
+ 
     return (
         
             <div>
             <div className='h-full w-full'>
                             <div className='flex flex-row w-full bg-zinc-900/60 p-3 rounded-full justify-between'>
-                                <div className='flex flex-row text-zinc-200 ml-3 items-center gap-6'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <div className='flex flex-row text-zinc-200 ml-3 items-center gap-6 w-full'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                     </svg>
                                     <input required type='text' id="name" name="name" placeholder='Type artist`s name'
-                                    className='p-1 font-geomatik bg-transparent placeholder-zinc-100/70  text-zinc-100 border-none outline-none'
+                                    className='p-1 font-geomatik bg-transparent placeholder-zinc-100/70 w-full  text-zinc-100 border-none outline-none'
                                     value={namea} onChange={(ev) => setNamea(ev.target.value)} />
                                 </div>
 
