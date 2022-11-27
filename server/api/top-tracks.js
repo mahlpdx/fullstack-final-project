b2796f708c046354c251ea915918faa2a226ef58
TimmyYoung <77657486+TimmyYoung@users.noreply.github.com>
2022-11-27 13:13:16 -0800

    Fixed backend for audio_features from v1

--- server/api/top-tracks.js
+++ server/api/top-tracks.js
@@ -44,8 +44,37 @@ topTracksRouter.get("/", async (req, res) => {
           };
 
           request.get(options, (error, response, body) => {
-            let ob = Object.assign(body);
-            res.send(ob);
+            tracks = body.tracks;
+            feature_ids = [];
+            for (let index = 0; index < body.tracks.length; index++) {
+              let track_id = body.tracks[index].id;
+              feature_ids.push(track_id);
+            }
+            feature_ids.join(',');
+            //End of copy.
+            
+             // GET request for top track audio features
+            let options = {
+                url: ` https://api.spotify.com/v1/audio-features?ids=${feature_ids}`,
+                headers: {
+                  Authorization: "Bearer " + token, 
+                },
+                json: true,};
+
+              request.get(options, (error, response, body) => {
+                console.log(tracks.length);
+                //Adding the audio_features object to top tracks//
+                for(let index = 0; index < tracks.length; index++){
+                    console.log(tracks[index].id);
+                    console.log(body.audio_features[index].id);
+                    if(tracks[index].id === body.audio_features[index].id){
+                        console.log("It works: " + index);
+                        Object.assign(tracks[index], {audio_features : body.audio_features[index]});
+                    }
+                }//End of Audio_features GET //
+                let ob = Object.assign(tracks);
+                res.send(ob);
+              });
           });
         } else {
           console.log("failed");
