4bdf6e006b89490bd00a03616503ee0399f8b074
TimmyYoung <77657486+TimmyYoung@users.noreply.github.com>
2022-11-28 14:17:49 -0800

    Fixed id bug for genres.

--- client/src/components/Searchpage/panels/genres.jsx
+++ client/src/components/Searchpage/panels/genres.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 
 
 function Genres ({genres}) {
+    let index = 1;
+
     return (
       <div className='w-full'>
         <div className='text-xl font-geomatik text-zinc-200/70 mb-3 ml-4 '>List of the genres the artist is associated with</div>
         <div className='flex flex-row items-center gap-6 font-geomatik text-zinc-200 w-full'>                  
           <div className='text-lg grid grid-cols-4 items-center gap-3'>
               {genres.map((genres) => (
-                  <div className='px-5 py-2 bg-indigo-600/60 rounded-full text-center '>{genres}</div>
+                
+                  <div className='px-5 py-2 bg-indigo-600/60 rounded-full text-center ' key = {genres}>{genres}</div>
+                    
               ),
               )}
           </div>
